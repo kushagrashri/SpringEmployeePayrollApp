ab79de8eb2e5b716cf6e8b06b78d3b199ddd553b
kushagrashri7 <kushagrashrisharma@gmail.com>
2025-03-05 00:04:21 +0530

    [Kushagra Sharma] add .UC06

--- src/main/java/com/EmployeePayroll/dto/EmployeeDTO.java
+++ src/main/java/com/EmployeePayroll/dto/EmployeeDTO.java
@@ -1,43 +1,10 @@
 package com.EmployeePayroll.dto;
-
+import lombok.Data;
+@Data
 public class EmployeeDTO {
     private Long id;
     private String name;
     private Double salary;
 
-    // Default constructor
-    public EmployeeDTO() {}
 
-    // Parameterized constructor
-    public EmployeeDTO(Long id , String name, Double salary) {
-        this.id=id;
-        this.name = name;
-        this.salary = salary;
-    }
-
-    // Getters and Setters
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public Double getSalary() {
-        return salary;
-    }
-
-    public void setSalary(Double salary) {
-        this.salary = salary;
-    }
 }
--- src/main/java/com/EmployeePayroll/model/Employee.java
+++ src/main/java/com/EmployeePayroll/model/Employee.java
@@ -4,7 +4,8 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-
+import  lombok.Data;
+@Data
 @Entity
 public class Employee {
     @Id
@@ -12,38 +13,5 @@ public class Employee {
     private Long  id;
     private String name;
     private Double salary;
-
-    public Employee(){}
-
-    public Employee(String name, Double salary){
-        this.name =name;
-
-        this.salary=salary;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public Double getSalary() {
-        return salary;
-    }
-
-    public void setSalary(Double salary) {
-        this.salary = salary;
-    }
-
 
 }
