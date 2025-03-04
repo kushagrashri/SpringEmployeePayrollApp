1f1314198e4318e8874a92114982ee5ebb9eb894
kushagrashri7 <kushagrashrisharma@gmail.com>
2025-03-04 16:41:48 +0530

    [Kushagra Sharma] add . UC02

--- src/main/java/com/EmployeePayroll/controllers/EmployeeController.java
+++ src/main/java/com/EmployeePayroll/controllers/EmployeeController.java
@@ -2,9 +2,12 @@ package com.EmployeePayroll.controllers;
 
 
 import com.EmployeePayroll.model.Employee;
-import com.EmployeePayroll.services.EmployeeService;
+import com.EmployeePayroll.repository.EmployeeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 import java.util.Optional;
@@ -14,35 +17,37 @@ import java.util.Optional;
 public class EmployeeController {
 
     @Autowired
-    private EmployeeService employeeService;
+    private EmployeeRepository employeeRepository;
 
     // Add a new employee
     @PostMapping()
     public Employee addEmployee(@RequestBody Employee employee) {
-        return employeeService.addEmployee(employee);
+        return employeeRepository.save(employee);
     }
 
     // Get all employees
     @GetMapping()
     public List<Employee> getAllEmployees() {
-        return employeeService.getAllEmployees();
+        return employeeRepository.findAll();
     }
 
     // Get employee by ID
     @GetMapping("/{id}")
     public Optional<Employee> getEmployeeById(@PathVariable Long id) {
-        return employeeService.getEmployeeById(id);
+        return employeeRepository.findById(id);
     }
 
     // Update an employee
     @PutMapping("/{id}")
     public Employee updateEmployee(@PathVariable Long id, @RequestBody Employee employeeDetails) {
-        return employeeService.updateEmployee(id, employeeDetails);
+        Employee employee = employeeRepository.findById(id).orElseThrow(() -> new RuntimeException("Employee not found"));
+        employee.setName(employeeDetails.getName());
+        employee.setDepartment(employeeDetails.getDepartment());
+        employee.setSalary(employeeDetails.getSalary());
+        return employeeRepository.save(employee);
     }
-
-    // Delete an employee
     @DeleteMapping("/{id}")
     public void deleteEmployee(@PathVariable Long id) {
-        employeeService.deleteEmployee(id);
+        employeeRepository.deleteById(id);
     }
 }
